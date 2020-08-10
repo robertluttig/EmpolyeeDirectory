1ec85d34fd1babb6c9623a145a2c816bc62cec83
Robert Luttig <robert.luttig@gmail.com>
2020-08-10 08:26:39 -0700

    added axios method to load people data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const[people,setPeople] = useState([]);
+
+    const loadPeople = () => {
+       axios.get(
+        "https://randomuser.me/api/?results=0&nat=us"
+      ).then((res)=>{
+        console.log(res.data.results);
+        const peopleData = res.data.results
+        setPeople(peopleData)
+    })
+  }
+  
+  
+
+
   return (
     <div className="App">
       <header>
         <h1>Page Deployed, Success!</h1>
+        <button onClick={loadPeople}>Clcik me!</button>
+        {people.map( (people)=>{
+          console.log(people);
+        })}
+
       </header>
     </div>
   );
