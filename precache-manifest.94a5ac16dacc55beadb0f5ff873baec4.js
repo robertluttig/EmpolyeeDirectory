71b4dc08ed3b1c7dadc9e965d808260d5ead8f3b
Robert Luttig <robert.luttig@gmail.com>
2020-08-10 17:23:16 -0700

    Updates

---
+++ precache-manifest.94a5ac16dacc55beadb0f5ff873baec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f483c3550539da856691fdcc196b9b",
+    "url": "/employeedirectory/index.html"
+  },
+  {
+    "revision": "ca1bdec3718185e732c4",
+    "url": "/employeedirectory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ca1bdec3718185e732c4",
+    "url": "/employeedirectory/static/js/2.1a219fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employeedirectory/static/js/2.1a219fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae9bd64fca75ee220af",
+    "url": "/employeedirectory/static/js/main.6eba8d49.chunk.js"
+  },
+  {
+    "revision": "6502cc3240a1235f4783",
+    "url": "/employeedirectory/static/js/runtime-main.8876b22a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employeedirectory/precache-manifest.d71aba06e51cc7ab493b05d601fb4706.js"
+  "/employeedirectory/precache-manifest.94a5ac16dacc55beadb0f5ff873baec4.js"
 );
 
 self.addEventListener('message', (event) => {
